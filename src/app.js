0e4fc0fa19ecc21d1da0dd01d9f2ab038b09d145
neeraj.maurya <neeraj.maurya@innovationm.com>
2023-12-23 12:08:08 +0530

    Add Refresh access token

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+app.use(cors({  // why use this line of code? // we are using cors middleware here to enable cors in our express app, so that our frontend can make requests to our backend., and we are also passing cors options to cors method., and we are also passing cors options to cors method.
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit: "16kb"})) // we are using express.json middleware here to parse json data from the request body, and we are also passing json options to express.json method, and we are also passing json options to express.json method.
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // why use this line of code? // we are using express.urlencoded middleware here to parse urlencoded data from the request body, and we are also passing urlencoded options to express.urlencoded method, and we are also passing urlencoded options to express.urlencoded method.
+app.use(express.static("public")) // why use this line of code? // we are using express.static middleware here to serve static files from the public folder, and we are also passing public folder path to express.static method, and we are also passing public folder path to express.static method.
+app.use(cookieParser()) // why use this line of code? // we are using cookieParser middleware here to parse cookies from the request header, and we are also passing cookieParser options to cookieParser method.
 
 
 //routes import
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,7 +24,7 @@ router.route("/login").post(loginUser)
 
 
 //secured routes
-router.route("/logout").post(verifyJWT,  logoutUser) // 
-
+router.route("/logout").post(verifyJWT,  logoutUser) // we are passing verifyJWT middleware function to logoutUser controller function as a second argument, so that logoutUser controller function can access req.user property, which is set by verifyJWT middleware function, and we are passing verifyJWT middleware function to logoutUser controller function as a second argument, so that logoutUser controller function can access req.user property, which is set by verifyJWT middleware function
+router.route("/refresh-token").post(refreshAccessToken) 
 
 export default router
